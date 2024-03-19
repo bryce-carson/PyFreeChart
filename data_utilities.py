a96f661a178c0eb8f57a9e25889e01de302294a5
Bryce Carson <bcars268@mtroyal.ca>
2024-03-19 12:28:34 -0600

    Add module commentary

--- data_utilities.py
+++ data_utilities.py
@@ -1,6 +1,16 @@
 """data_utilities.py is a Pythonic implementation of the DataUtilities class in
-JFreeChart."""
-from numbers import Real
+JFreeChart.
+
+In JFreeChart, the Number abstract base class is used to retrieve numeric
+values from data arguments provided to the methods of DataUtilties. The
+retrieved value is always converted to a double before operating upon it. In
+this Python translation there is no need to use the Number ABC because Python
+is duck-typed, so any value returned by methods which would in principle
+operate upon rows and columns of a two-dimensional data structure would not
+need to be cast to a higher type in the Numeric hierarchy.
+
+"""
+from numbers import Real  # NOTE: not needed; see the above explanation.
 
 
 class DataUtilities:
@@ -22,21 +32,15 @@ class DataUtilities:
             total += column
         return total
 
+    # NOTE: there is no need to implement these methods because of Python's
+    # duck-typing.
     @classmethod
     def create_number_array(cls, data: [float]) -> [Real]:
-        """Coerce the float elements of a one-dimensional list to numbers.
-
-        This uses the numeric abstract base class to permit: TODO: determine
-        why JFreeChart uses this in DataUtilities.
-
-        """
+        """Coerce the float elements of a one-dimensional list to numbers."""
 
     @classmethod
     def create_number_array_2d(cls, data: [[float]]) -> [[Real]]:
-        """Coerce the float elements of a two-dimensional list to numbers.
-
-        TODO: see the one-dimensional method.
-        """
+        """Coerce the float elements of a two-dimensional list to numbers."""
 
     @classmethod
     def calculate_cumulative_percentages(cls, data: [float]):
